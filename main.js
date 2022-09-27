605136c4df15369a230137755591f1d776882d69
Nadia Marsal <99929133+Carixa@users.noreply.github.com>
2022-09-26 23:26:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let age = 13;
+let height = 120; //en esta parte se quedan los datos tomados por la aplicacion creada para medir y obtener la edad del usuario .
+
+     if (age > 10 && height >= '100') { //Acá se determina como requisito que la edad sea mayor a 10 años y la altura igual o mayor a 1 metro/100cm
+    console.log ('Puede subir a la atracción'); //En caso de cumplir los requisitos se ejecuta el mensaje/orden indicando que puede subir a la montaña rusa
+} else { //En caso que no se ciumpla se ejecuta la orden indicando que no puede
+    console.log ('No puede subir.');
+}
